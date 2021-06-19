84b327be259867025c83a65e086bb8071a537453
Nikita Morozov <morozov.space@mail.ru>
2021-06-19 20:02:20 +0300

    Default styling

--- services/frontend/app/modules/styles.js
+++ services/frontend/app/modules/styles.js
@@ -1,9 +1,8 @@
 export default {
   scss: [
     "@/assets/styles/_reset.scss",
-    "@/assets/styles/_variables.scss",
-    "@/assets/styles/_fonts.scss",
-    "@/assets/styles/_mixins.scss",
-    "@/assets/styles/_themes.scss",
+    "@/assets/styles/_vars/*.scss",
+    "@/assets/styles/_mixins/*.scss",
+    "@/assets/styles/themes.scss",
   ],
 }
