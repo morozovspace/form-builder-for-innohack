7d4b73c67faf8a622d7b9e103f007e51c4816236
Nikita Morozov <morozov.space@mail.ru>
2021-06-19 21:45:19 +0300

    Add swagger to backend

---
+++ services/backend/app/src/endpoint.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+    app.get('/users/:id', (req, res) => {
+	    
+	const filtro = req.query.filtro
+
+        return res.status(404).send(false)
+
+    })
+}
--- services/backend/app/src/server.ts
+++ services/backend/app/src/server.ts
@@ -1,8 +1,18 @@
 import express from "express"
-
 const app = express()
-const { PORT } = process.env
+const PORT = 3001
 app.get('/', (request, response) => {
   response.send('Hello world!');
-});
+})
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+
+const http = require('http')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+http.createServer(app).listen(3000)
+console.log("Listening at:// port:%s (HTTP)", 3000)
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+require('./endpoints')(app)
